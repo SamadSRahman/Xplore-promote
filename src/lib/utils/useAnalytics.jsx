da8e03e3e3b532f12eb7ddea5370cfe5c909eac0
BhavyaApprikart <bhavya@apprikart.com>
2024-12-13 15:10:33 +0530

    url changed for anakytics

--- src/lib/utils/useAnalytics.jsx
+++ src/lib/utils/useAnalytics.jsx
@@ -6,8 +6,15 @@ export default function useAnalytics() {
     const token = localStorage.getItem('accessToken');
     const channel = localStorage.getItem('channel');
 
+    let API_BASE_URL = 'https://pre.xplore.xircular.io/api'; 
+    if(window.location.origin==="https://xplr.live"){
+        console.log(window.location.origin);  
+     API_BASE_URL = 'https://xplr.live/api';
+    }
+
+    
     const getAnalyticsData = async (campaignId) => {
-        const response = await fetch(`https://pre.xplore.xircular.io/api/v1/user/getSubmittedContact/${campaignId}`,
+        const response = await fetch(`${API_BASE_URL}v1/user/getSubmittedContact/${campaignId}`,
             {
                 headers: {
                     Authorization: `Bearer ${token}`,
